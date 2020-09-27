030ff3ecb7f4e2d25f366e1598da812c939a1d16
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-27 19:03:27 +0200

    create full order formular, add post method in PizzaCreatorController to send order

--- src/main/java/com/ludynia/pizza/controller/OrderController.java
+++ src/main/java/com/ludynia/pizza/controller/OrderController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,5 +18,10 @@ public class OrderController {
     public String orderForm(Model model){
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+    @PostMapping
+    public String processOrder(Order order){
+        log.info("The following order has been placed "+ order);
+        return "redirect:/";
     }
 }
--- src/main/java/com/ludynia/pizza/controller/PizzaCreatorController.java
+++ src/main/java/com/ludynia/pizza/controller/PizzaCreatorController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.stream.Collectors;
@@ -58,5 +59,8 @@ public class PizzaCreatorController {
                 .collect(Collectors.toList());
 
 
+    } @PostMapping
+    public String processCreator(){
+        return "redirect:/orders/current";
     }
 }
