a7c7f4a476232d54f92ab5e9daf536ca68bb681a
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-28 10:51:00 +0200

    add constraint to pizza class

--- src/main/java/com/ludynia/pizza/model/Pizza.java
+++ src/main/java/com/ludynia/pizza/model/Pizza.java
@@ -2,10 +2,15 @@ package com.ludynia.pizza.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Pizza {
+    @NotNull
+    @Size(min = 3, message = "Name should contain at least 3 letters")
     private String name;
+    @Size(min = 1, message = "Please choose at least 1 ingredient")
     private List<Ingredient>ingredientList;
 }
