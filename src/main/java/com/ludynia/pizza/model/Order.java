479798409105d06d31778a513feafcc8b0cb0b3b
Piotr Ludynia <pietrekpl18@gmail.com>
2020-09-30 21:34:16 +0200

    prepare class ingredient, pizza order for persistence add h2 properties in application properties

--- src/main/java/com/ludynia/pizza/model/Order.java
+++ src/main/java/com/ludynia/pizza/model/Order.java
@@ -1,14 +1,27 @@
 package com.ludynia.pizza.model;
 
 import lombok.Data;
+import lombok.Generated;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "pizza_order")
+public class Order implements Serializable {
+
+    private static final long serialUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @NotBlank(message = "Field firstName and LastName is mandatory")
     private String name;
     @NotBlank(message = "Field street is mandatory")
@@ -25,4 +38,19 @@ public class Order {
     private String ccExpiration;
     @Digits(integer = 3,fraction = 0,message = "CVV number is not correct")
     private String ccCVV;
+    private Date created;
+
+    @PrePersist
+    public void created(){
+        this.created = new Date();
+    }
+
+    @ManyToMany(targetEntity = Pizza.class)
+    private List<Pizza>pizzas = new ArrayList<>();
+
+    public void addCreator(Pizza creator){
+        this.pizzas.add(creator);
+    }
+
+
 }
--- src/main/java/com/ludynia/pizza/model/Pizza.java
+++ src/main/java/com/ludynia/pizza/model/Pizza.java
@@ -2,15 +2,31 @@ package com.ludynia.pizza.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotNull
     @Size(min = 2, message = "Name should contain at least 2 letters")
     private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min = 1, message = "Please choose at least 1 ingredient")
     private List<String>ingredientList;
+
+    private Date created;
+
+    @PrePersist
+    void created () {
+        this.created = new Date();
+    }
 }
