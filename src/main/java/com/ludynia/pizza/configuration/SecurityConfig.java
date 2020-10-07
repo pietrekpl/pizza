4c4acbbd073389c6a455e774e011c8fed4358f42
Piotr Ludynia <pietrekpl18@gmail.com>
2020-10-07 22:21:31 +0200

    fix security to enter the app in browser

--- src/main/java/com/ludynia/pizza/configuration/SecurityConfig.java
+++ src/main/java/com/ludynia/pizza/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ludynia.pizza.configuration;
 
 
+import com.ludynia.pizza.service.UserDetailsRepositoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -21,8 +23,30 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+   private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/creator").authenticated()
+                .mvcMatchers("/login").permitAll()
+                .mvcMatchers("/h2/**").permitAll()
+                .and().formLogin();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    /*    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
     }
@@ -33,5 +57,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .roles("ADMIN")
                 .password("{noop}password");
-    }
+    }*/
 }
--- src/main/java/com/ludynia/pizza/repository/UserRepository.java
+++ src/main/java/com/ludynia/pizza/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.ludynia.pizza.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User,Long> {
 
