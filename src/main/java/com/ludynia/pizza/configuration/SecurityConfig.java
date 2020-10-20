0996222507abd9d460ddb9b423cc828076271305
Piotr Ludynia <pietrekpl18@gmail.com>
2020-10-20 18:47:55 +0200

    add password encoder, Fix access to h2 database

--- src/main/java/com/ludynia/pizza/configuration/SecurityConfig.java
+++ src/main/java/com/ludynia/pizza/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ludynia.pizza.configuration;
 
 
 import com.ludynia.pizza.model.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,12 +13,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 
     @Override
@@ -32,11 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").authenticated()
-                .antMatchers("/creator","/order").permitAll()
+                .antMatchers("/creator","/order","/h2-console/**").permitAll()
                 .and()
                 .httpBasic()
                 .and()
                 .csrf()
                 .disable();
+                http.headers().frameOptions().disable();
     }
 }
