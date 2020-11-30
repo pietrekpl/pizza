59deabd33c1d5f2f318761b460ed9ab375d1f1df
Piotr Ludynia <pietrekpl18@gmail.com>
2020-11-30 18:38:06 +0100

    move data to messages.properties

--- src/main/java/com/ludynia/pizza/twilio/TwilioConfig.java
+++ src/main/java/com/ludynia/pizza/twilio/TwilioConfig.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class TwilioConfig {
 
 
-    private final static String ACCOUNT_SID = "AC00b0e4326643214d0bf708081d817ce6";
-    private final static String AUTH_ID = "3d0a26be47e95f5dfab982898f1d0de9";
-    private final static String PHONE_NUMBER = "+16204008915";
+    private final static String ACCOUNT_SID = "sid";
+    private final static String AUTH_ID = "auth_token";
+    private final static String PHONE_NUMBER = "phone_number";
 
     private String accountSid;
     private String authToken;
